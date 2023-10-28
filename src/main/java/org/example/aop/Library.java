ce90ecfea08321f9b97ffe02e5c86c25c5c9be90
EmilK102 <vip.elick@gmail.com>
2023-10-28 17:34:03 +0500

    first aspect class

--- src/main/java/org/example/aop/Library.java
+++ src/main/java/org/example/aop/Library.java
@@ -1,7 +1,10 @@
 package org.example.aop;
 
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
 public class Library {
     public void getBook(){
-
+        System.out.println("Get Book");
     }
 }
---
+++ src/main/java/org/example/aop/aspect/LoggingAspect.java
@@ -0,0 +1,15 @@
+package org.example.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice(){
+        System.out.println("!Before get Book method!");
+    }
+}
