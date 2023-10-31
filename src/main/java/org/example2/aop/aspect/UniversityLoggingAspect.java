9774cc66b2c8a2820df544f584f97c88a043b1c0
EmilK102 <vip.elick@gmail.com>
2023-10-31 20:04:33 +0500

    after returning 2

--- src/main/java/org/example2/aop/aspect/UniversityLoggingAspect.java
+++ src/main/java/org/example2/aop/aspect/UniversityLoggingAspect.java
@@ -3,7 +3,10 @@ package org.example2.aop.aspect;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.example2.aop.Student;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Aspect
@@ -15,8 +18,12 @@ public class UniversityLoggingAspect {
                 "списка студентов перед выполнением метода getStudents");
     }
 
-    @AfterReturning("org.example2.aop.aspect.MyPointcuts.allGetStudentsMethods()")
-    private void afterGetStudentsAdvice(){
+    @AfterReturning(pointcut = "org.example2.aop.aspect.MyPointcuts.allGetStudentsMethods()",
+    returning = "students")
+    private void afterGetStudentsAdvice(List<Student> students){
+        Student student = new Student("Now", 44, 23.2);
+        students.add(student);
+
         System.out.println("afterGetStudentsAdvice: логируем получение " +
                 "списка студентов после успешного выполнения метода getStudents");
     }
--- src/test/java/org/example2/aop/Test.java
+++ src/test/java/org/example2/aop/Test.java
@@ -2,12 +2,15 @@ package org.example2.aop;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         University university = context.getBean("university", University.class);
         university.addStudent();
-        university.getStudents();
+        List<Student> students = university.getStudents();
+        System.out.println(students);
         context.close();
     }
 }
